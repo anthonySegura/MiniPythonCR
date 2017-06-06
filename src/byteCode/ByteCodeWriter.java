a7f6d3686b6b72e1da0da4f63be3625c1e542d35
Anthony Segura <ansechaves@hotmail.com>
2017-06-05 19:23:46 -0600

    Revisar error

---
+++ src/byteCode/ByteCodeWriter.java
@@ -0,0 +1,17 @@
+package byteCode;
+
+import java.util.List;
+
+/**
+ * Created by anthony on 01/06/17.
+ * Clase que genera el archivo con el ByteCode
+ */
+public class ByteCodeWritter {
+
+    public static void generate(List<Instruction> instructions){
+
+
+
+    }
+
+}
